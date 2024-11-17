073caee90300cc2a693a63f95d7b72d3e92b8d37
beimingxue00 <167213262+beimingxue00@users.noreply.github.com>
2024-11-17 20:02:24 +0800

    Update WrongView.axaml.cs

--- dpa/dpa/Views/WrongView.axaml.cs
+++ dpa/dpa/Views/WrongView.axaml.cs
@@ -7,5 +7,8 @@ namespace dpa.Views;
 public partial class WrongView : UserControl {
     public WrongView() {
         InitializeComponent();
+        // 创建并绑定 ViewModel
+            IPreferenceStorage preferenceStorage = new FilePreferenceStorage();
+            this.DataContext = new WrongViewModel(new PoetryStorage(preferenceStorage));
     }
 }
