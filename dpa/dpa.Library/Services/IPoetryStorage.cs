4ec7468a326126ba2158bb766ca1f39bd34093e9
beimingxue00 <167213262+beimingxue00@users.noreply.github.com>
2024-11-17 19:54:16 +0800

    Update IPoetryStorage.cs

--- dpa/dpa.Library/Services/IPoetryStorage.cs
+++ dpa/dpa.Library/Services/IPoetryStorage.cs
@@ -12,4 +12,7 @@ public interface IPoetryStorage {
 
     Task<IList<Poetry>> GetPoetriesAsync(
         Expression<Func<Poetry, bool>> where, int skip, int take);
+    
+    // 这是修改后的用于返回问题字段的方法
+    Task<List<string>> GetExerciseQuestionsAsync(Expression<Func<Exercise, bool>> where, int skip, int take);
 }
