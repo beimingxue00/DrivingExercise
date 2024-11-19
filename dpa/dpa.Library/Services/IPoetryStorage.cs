657de41a35aa03d953b2d8edcce4dd2f0d791162
beimingxue00 <167213262+beimingxue00@users.noreply.github.com>
2024-11-19 22:31:32 +0800

    Update IPoetryStorage.cs

--- dpa/dpa.Library/Services/IPoetryStorage.cs
+++ dpa/dpa.Library/Services/IPoetryStorage.cs
@@ -4,7 +4,7 @@ using dpa.Library.Models;
 namespace dpa.Library.Services;
 
 public interface IPoetryStorage {
-    bool IsInitialized { get; }
+   bool IsInitialized { get; }
 
     Task InitializeAsync();
 
@@ -14,5 +14,5 @@ public interface IPoetryStorage {
         Expression<Func<Poetry, bool>> where, int skip, int take);
     
     // 这是修改后的用于返回问题字段的方法
-    Task<List<string>> GetExerciseQuestionsAsync(Expression<Func<Exercise, bool>> where, int skip, int take);
+    Task<List<Exercise>> GetExerciseQuestionsAsync(Expression<Func<Exercise, bool>> where, int skip, int take);
 }
