24843218661dcd0618fe9b7b30165fd65ceaa58c
beimingxue00 <167213262+beimingxue00@users.noreply.github.com>
2024-11-19 19:03:51 +0800

    Update MenuNavigationService.cs

--- dpa/dpa/Services/MenuNavigationService.cs
+++ dpa/dpa/Services/MenuNavigationService.cs
@@ -5,17 +5,28 @@ using dpa.Library.ViewModels;
 namespace dpa.Services;
 
 public class MenuNavigationService : IMenuNavigationService {
-    public void NavigateTo(string view) {
-        ViewModelBase viewModel = view switch {
-            MenuNavigationConstant.AnswerView => ServiceLocator.Current
-                .AnswerViewModel,
-            MenuNavigationConstant.WrongView => ServiceLocator.Current
-                .WrongViewModel,
-            MenuNavigationConstant.ProgressView => ServiceLocator.Current
-                .ProgressViewModel,
-            _ => throw new Exception("未知的视图。")
-        };
-
-        ServiceLocator.Current.MainViewModel.SetMenuAndContent(view, viewModel);
+   public void NavigateTo(string view)
+    {
+        ViewModelBase viewModel=null;
+        switch (view)
+        {
+            case MenuNavigationConstant.AnswerView:
+                ServiceLocator.Current.AnswerViewModel.AdviseInitial();
+                viewModel = ServiceLocator.Current
+                    .AnswerViewModel;
+                break;
+            case MenuNavigationConstant.WrongView:
+                viewModel = ServiceLocator.Current
+                    .WrongViewModel;
+                break;
+            case MenuNavigationConstant.ProgressView:
+                viewModel = ServiceLocator.Current
+                    .ProgressViewModel;
+                break;
+        }
+        if (viewModel != null)
+        {
+            ServiceLocator.Current.MainViewModel.SetMenuAndContent(view, viewModel);
+        }
     }
 }
